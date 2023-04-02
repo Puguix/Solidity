80ea138156810b2868fcb1646904745a001efb35
Paul GAUTHIER <paul.gauthier@grenoble-inp.org>
2023-04-02 17:07:23 +0200

    init challenge 4

--- challenge-5-multisig/packages/react-app/src/helpers/TransactionParser.js
+++
@@ -1,9 +0,0 @@
-
-export default parseTransaction(readContracts, contractName, transactionData){
-    const transactionInfo = readContracts[contractName].interface.parseTransaction(transactionData);
-
-    const {name, payable, stateMutability, type, inputs, args} = transactionInfo.functionFragment;
-    
-
-
-}

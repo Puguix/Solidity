617bff701aa882b8aa8cc7c8036972675a45a7a9
Paul GAUTHIER <paul.gauthier@grenoble-inp.org>
2023-04-02 17:01:21 +0200

    end of challenge 3

--- challenge-3-dice-game/packages/hardhat/deploy/01_deploy_riggedRoll.js
+++ challenge-3-dice-game/packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -9,19 +9,17 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const diceGame = await ethers.getContract("DiceGame", deployer);
 
-  /*
   await deploy("RiggedRoll", {
-   from: deployer,
-   args: [diceGame.address],
-   log: true,
+    from: deployer,
+    args: [diceGame.address],
+    log: true,
   });
-  */
 
-  //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+  const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-  //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
-  
-
+  const ownershipTransaction = await riggedRoll.transferOwnership(
+    "0x66895cF194df3532A24BB1caE928734C478b7D27" // change local address here
+  );
 };
 
 function sleep(ms) {
